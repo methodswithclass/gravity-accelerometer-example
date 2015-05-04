9222eca266bbaba30b6ae1c12f3888c588759583
methodswithclass <chris@methodswithclass.com>
2015-05-04 10:13:41 -0400

    cleaning up

--- js/accelerometer.js
+++ js/accelerometer.js
@@ -51,9 +51,9 @@ var Accelerometer = function (params, obj) {
 
 	var self = this;
 	
-	this.showConsole = false;
+	var showConsole = false;
 	
-	this.interval = params.interval;
+	var interval = params.interval;
 	
 	this.getBounds = function () {
 	
@@ -69,185 +69,88 @@ var Accelerometer = function (params, obj) {
 	var filterBucket = [];
 	var filterSize = params.filterSize;
 
+	var factor;
+	var xDir;
+	var yDir;
 	var threshold;
 	
-	this.unfiltered = new Vector(0,0,0);
-	this.accel0 = new Vector(0,0,0);
-	this.vel0 = new Vector(0,0,0);
-	this.vel1 = new Vector(0,0,0);
-	this.pos0 = new Vector(0,0,0);
-	this.pos1 = new Vector(0,0,0);
-	this.startTime = 0;
+	var unfiltered = new Vector(0,0,0);
+	var accel1;
+	var accel0 = new Vector(0,0,0);
+	var vel0 = new Vector(0,0,0);
+	var vel1 = new Vector(0,0,0);
+	var pos0 = new Vector(0,0,0);
+	var pos1 = new Vector(0,0,0);
+	var startTime = 0;
 	
-	this.position = {x:this.pos0.x, y:this.pos0.y};
-	this.velocity = {x:this.vel0.x, y:this.vel0.y};
+	var position = {x:pos0.x, y:pos0.y};
+	var velocity = {x:vel0.x, y:vel0.y};
 	
-	this.mu = params.mu;
-	this.damp = params.damp;
-	
-	this.running = false;
+	var mu = params.mu;
+	var damp = params.damp;
 
-	this.setValues = function () {
+	var running = false;
+	var timer;
 
-		this.factor = globals.factor*params.factor;
-		this.xDir = globals.xDir;
-		this.yDir = globals.yDir;
-		threshold = this.factor*0.5;
+	var setValues = function () {
+
+		factor = globals.factor*params.factor;
+		xDir = globals.xDir;
+		yDir = globals.yDir;
+		threshold = factor*0.5;
 
 	}
 	
-	this.motion = function (e) {
+	var bounce = function () {
 		
-		//con.log("motion");
+		var sideX = pos1.x/Math.abs(pos1.x);
 		
-		if (self.running) {
-			
-			//con.log("motion on");
-			
-			if (params.gravity) {
-				self.unfiltered.x = self.xDir*self.factor*e.accelerationIncludingGravity.x;
-				self.unfiltered.y = self.yDir*self.factor*e.accelerationIncludingGravity.y;
-			}
-			else {
-				self.unfiltered.x = self.xDir*self.factor*e.acceleration.x;
-				self.unfiltered.y = self.yDir*self.factor*e.acceleration.y;
-			}
-			
-			self.unfiltered.time = (e.timeStamp - self.startTime)/1000;
-			
-			if (self.showConsole)
-				con.log("unfiltered: " + self.unfiltered.printValues());
-		}
-	}
-	
-	this.start = function () {
+		var minVel = 12*(Math.abs(accel1.y)+Math.abs(accel1.x));
 		
-		console.log("running");
-
-		self.setValues();
-		
-		self.running = true;
-		
-		var date = new Date();
-		self.startTime = date.getTime();
-		
-		self.timer = setInterval(function () {
-			
-			filterBucket[filterBucket.length] = self.unfiltered;
-				
-			if (filterBucket.length == filterSize) {
-				
-				self.integrate(filterBucket);
-				
-				filterBucket = [];	
-			}
-				
-		}, 1000*self.interval);
-	}
-	
-	this.stop = function () {
-		
-		con.log("stopped");
-		
-		self.running = false;
-		
-		if (self.timer) {
-			clearInterval(self.timer);	
-		}
-		
-		//self.reset();
-	}
-	
-	this.integrate = function (accelArray) {
-		
-		self.accel1 = utility.average(accelArray);
-		
-		if (self.accel1.len() < threshold) {
-			self.accel1 = new Vector(0,0,self.accel1.time);
-		}
-		
-		var timeInterval = 1000*self.interval*filterSize;
-		
-		self.vel1 = self.vel0.add(self.accel0.multiply(timeInterval)).add(self.accel1.subtract(self.accel0).multiply(0.5*timeInterval));
-		
-		self.pos1 = self.pos0.add(self.vel0.multiply(timeInterval)).add(self.vel1.subtract(self.vel0).multiply(0.5*timeInterval));
-		
-		self.bounce();
-		
-		self.friction();
-		
-		/*if (self.showConsole)
-			self.logOnInterval();*/
-		
-		self.updateMotion(self.pos1, self.vel1, self.accel1);
-		
-		self.pos0 = self.pos1;
-		self.vel0 = self.vel1;
-		self.accel0 = self.accel1;
-		
-	}
-	
-	this.bounce = function () {
-		
-		var sideX = self.pos1.x/Math.abs(self.pos1.x);
-		
-		var minVel = 12*(Math.abs(self.accel1.y)+Math.abs(self.accel1.x));
-		
-		if (Math.abs(self.pos1.x) >= self.xMax) {
-			self.pos1.x	= sideX*self.xMax;
-			self.vel1.x = -(1-self.damp)*self.vel1.x;
-			if (Math.abs(self.vel1.x) < minVel && params.gravity) {
-				self.vel1.x = 0;	
+		if (Math.abs(pos1.x) >= self.xMax) {
+			pos1.x	= sideX*self.xMax;
+			vel1.x = -(1-damp)*vel1.x;
+			if (Math.abs(vel1.x) < minVel && params.gravity) {
+				vel1.x = 0;	
 			}
 		}
 		
 		
-		var sideY = self.pos1.y/Math.abs(self.pos1.y);
+		var sideY = pos1.y/Math.abs(pos1.y);
 		
-		if (Math.abs(self.pos1.y) >= self.yMax) {
-			self.pos1.y	= sideY*self.yMax;
-			self.vel1.y = -(1-self.damp)*self.vel1.y;
-			if (Math.abs(self.vel1.y) < minVel && params.gravity) {
-				self.vel1.y = 0;
+		if (Math.abs(pos1.y) >= self.yMax) {
+			pos1.y	= sideY*self.yMax;
+			vel1.y = -(1-damp)*vel1.y;
+			if (Math.abs(vel1.y) < minVel && params.gravity) {
+				vel1.y = 0;
 			}
 		}
 			
 	}
 	
-	this.friction = function () {
+	var friction = function () {
 		
-		if (self.accel1.len() == 0) {
-			self.vel1 = self.vel1.multiply(1-self.mu);	
+		if (accel1.len() == 0) {
+			vel1 = vel1.multiply(1-mu);	
 		}
 	}
 	
 	
 	
-	this.reset = function () {
+	var reset = function () {
 		
 		filterBucket = [];
 		
-		self.unfiltered = new Vector(0,0,0);
-		self.accel0 = new Vector(0,0,0);
-		self.vel0 = new Vector(0,0,0);
-		self.pos0 = new Vector(0,0,0);
-		self.startTime = 0;
+		unfiltered = new Vector(0,0,0);
+		accel0 = new Vector(0,0,0);
+		vel0 = new Vector(0,0,0);
+		pos0 = new Vector(0,0,0);
+		startTime = 0;
 		
-		self.updateMotion(self.pos0, self.vel0);	
+		updateMotion(self.pos0, self.vel0);	
 	}
-	
-	this.logOnInterval = function () {
-		
-		if (self.accel1.len() > 0) {
-			con.log("accel: " + self.accel1.printValues() + " len: " + self.accel1.len());
-			//self.log("vel: " + self.vel1.printValues());
-			con.log("pos: " + self.pos1.printValues());
-			
-			con.log(" ");
-		}
-	}
-	
-	this.updateMotion = function (pos, vel, acc) {
+
+	var updateMotion = function (pos, vel, acc) {
 		
 		//con.log("update " + xPos + " " + yPos);
 		
@@ -255,6 +158,110 @@ var Accelerometer = function (params, obj) {
 		
 		window.dispatchEvent(evt);
 			
+	}
+
+	var integrate = function (accelArray) {
+		
+		accel1 = utility.average(accelArray);
+		
+		if (accel1.len() < threshold) {
+			accel1 = new Vector(0,0,accel1.time);
+		}
+		
+		var timeInterval = 1000*interval*filterSize;
+		
+		vel1 = vel0.add(accel0.multiply(timeInterval)).add(accel1.subtract(accel0).multiply(0.5*timeInterval));
+		
+		pos1 = pos0.add(vel0.multiply(timeInterval)).add(vel1.subtract(vel0).multiply(0.5*timeInterval));
+		
+		bounce();
+		
+		friction();
+		
+		/*if (self.showConsole)
+			self.logOnInterval();*/
+		
+		updateMotion(pos1, vel1, accel1);
+		
+		pos0 = pos1;
+		vel0 = vel1;
+		accel0 = accel1;
+		
+	}
+	
+	var logOnInterval = function () {
+		
+		if (accel1.len() > 0) {
+			con.log("accel: " + accel1.printValues() + " len: " + accel1.len());
+			//self.log("vel: " + self.vel1.printValues());
+			con.log("pos: " + pos1.printValues());
+			
+			con.log(" ");
+		}
+	}
+
+	this.motion = function (e) {
+		
+		//con.log("motion");
+		
+		if (running) {
+			
+			//con.log("motion on");
+			
+			if (params.gravity) {
+				unfiltered.x = xDir*factor*e.accelerationIncludingGravity.x;
+				unfiltered.y = yDir*factor*e.accelerationIncludingGravity.y;
+			}
+			else {
+				unfiltered.x = xDir*factor*e.acceleration.x;
+				unfiltered.y = yDir*factor*e.acceleration.y;
+			}
+			
+			unfiltered.time = (e.timeStamp - startTime)/1000;
+			
+			if (showConsole)
+				con.log("unfiltered: " + unfiltered.printValues());
+		}
+	}
+
+	
+	
+	this.start = function () {
+		
+		console.log("running");
+
+		setValues();
+		
+		running = true;
+		
+		var date = new Date();
+		startTime = date.getTime();
+		
+		timer = setInterval(function () {
+			
+			filterBucket[filterBucket.length] = unfiltered;
+				
+			if (filterBucket.length == filterSize) {
+				
+				integrate(filterBucket);
+				
+				filterBucket = [];	
+			}
+				
+		}, 1000*interval);
+	}
+	
+	this.stop = function () {
+		
+		con.log("stopped");
+		
+		running = false;
+		
+		if (timer) {
+			clearInterval(timer);	
+		}
+		
+		//self.reset();
 	}
 	
 	this.getMotion = function (func) {
@@ -267,11 +274,7 @@ var Accelerometer = function (params, obj) {
 
 	this.getAccel = function () {
 
-		return self.accel1;
-	}
-	
-	this.listTime = function () {
-		self.obj.innerHTML = self.acceleration.time;	
+		return accel1;
 	}
 		
 }
