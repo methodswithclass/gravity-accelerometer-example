22270b852e892e17270782c64c09bcd8fa2a8e79
Christopher Polito <christopher.polito343@gmail.com>
2016-10-23 15:03:26 -0400

    update

--- index.php
+++ index.php
@@ -4,67 +4,75 @@
 
 	<link rel="stylesheet" href="http://code.methodswithclass.com/api/classes.css">
 
-	<script src="js/jquery-1.11.3.min.js"></script>
+	<script src="http://code.methodswithclass.com/api/accelerometer-1.js"></script>
+	<!-- <script src="js/accelerometer-1.js"></script> -->
 
 	<script>
 
-		//accelerometer calculation and integration parameters
-		var params = {
-			interval:2,
-			filterSize:3,
-			factor:0.8,
-			mu:0.1,
-			damp:0.4,
-			gravity:true,
-			bounce:true
-		}
+		(function () {
 
-		//object parameters, shape is only dependent value, other values can be used to change how the object looks, or it can be hardcoded
-		var objParams = {
-			shape:"circle",
-			size:200,
-			color:"black"
-		}
+			setTimeout(function () {
 
-		$(document).ready(function () {
+				//accelerometer calculation and integration parameters
+				var params = {
+					interval:2,
+					filterSize:3,
+					factor:0.8,
+					mu:0.1,
+					damp:0.4,
+					gravity:true,
+					bounce:true
+				}
 
-			var g = mcaccel.utility;
+				//object parameters, shape is only dependent value, other values can be used to change how the object looks, or it can be hardcoded
+				var objParams = {
+					shape:"circle",
+					size:200,
+					color:"black"
+				}
 
-			//these values must be set per session on different devices through some calibration means
-			g.setFactor(g.const.factorG, 0.01);
-			g.setAxis(g.const.x, -1);
-			g.setAxis(g.const.y, 1);
+				var g = mcaccel.utility;
 
-			var obj = new mcaccel.object({
-				id:"object",
-				object:$("#object")[0],
-				params:objParams
-			});
-				
-			var accel = new mcaccel.accelerometer({
-				id:"accel",
-				object:obj,
-				params:params
-			});
+				//these values must be set per session on different devices through some calibration means
+				g.setFactor(g.const.factorG, 0.01);
+				g.setAxis(g.const.x, -1);
+				g.setAxis(g.const.y, 1);
 
-			accel.getMotion("accel", function (id, pos, vel, acc) {
+				var object = document.getElementById("object");
 
-				console.log("set position", id, pos.x, "   ", pos.y);
+				var obj = new mcaccel.object({
+					id:"object",
+					object:object,
+					params:objParams
+				});
+					
+				var accel = new mcaccel.accelerometer({
+					id:"accel",
+					object:obj,
+					params:params
+				});
 
-				obj.setPosition(pos);
-				obj.setVelocity(vel);
-				obj.setAcceleration(acc);
+				accel.getMotion("accel", function (id, pos, vel, acc) {
 
-			});
+					console.log("set position", id, pos.x, "   ", pos.y);
 
-			//rubber meets road
-			window.ondevicemotion = accel.motion;
+					obj.setPosition(pos);
+					obj.setVelocity(vel);
+					obj.setAcceleration(acc);
 
-			//begin integrating for device position and updating position of object
-			accel.reset();
-			accel.start();
+				});
 
-		});
+				//rubber meets road
+				window.ondevicemotion = accel.motion;
+
+				//begin integrating for device position and updating position of object
+				accel.reset();
+				accel.start();
+
+
+			}, 500);
+
+		})();
 
 	</script>
 
@@ -77,9 +85,6 @@
 			<div id="object"></div>
 		</div>
 	</div>
-
-	<script src="http://code.methodswithclass.com/api/accelerometer-1.js"></script>
-	<!-- <script src="js/accelerometer-1.js"></script> -->
 
 </body>
 
